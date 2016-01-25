b50213bd2405450170ed04712b170853387eeb07
Emanuel <emanuelsab@gmail.com>
2016-01-25 08:45:29 -0430

    Initial commit:operations basics ADD,SUB,AND,OR,XOR

---
+++ alu.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//==================================================================================================
+//  Filename      : alu.v
+//  Created On    : Tue Jan  19 06:40:00 2016
+//  Last Modified : 
+//  Revision      : 0.1
+//  Author        : Emanuel Sánchez & Ninisbeth 
+//  Company       : Universidad Simón Bolívar
+//  Email         : emanuelsab@gmail.com & 
+//
+//  Description   : The Execution unit.
+//                  Performs the following operations:
+//                  - Arithmetic    (lista)
+//                  - Logical			
+//                  - Shift			
+//                  - Comparison		
+//==================================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////////////
+`include "definitions.v"
+
+module alu(
+	 input [31:0] 				data_a,
+    input [31:0] 				data_b,
+    input [2:0]				operation,
+    output reg [31:0] 		alu_result
+    );
+
+//--------------------------------------------------------------------------
+// Operation Select
+//--------------------------------------------------------------------------
+	always @(*) begin
+		case (operation)
+			`OP_ADD  : alu_result = (data_a + data_b) & `MAX_RANGE;
+			`OP_SUB  : alu_result = (data_a - data_b) & `MAX_RANGE;
+			`OP_AND  : alu_result = data_a & data_b;
+			`OP_OR   : alu_result = data_a | data_b;
+			`OP_XOR  : alu_result = data_a ^ data_b;
+			`OP_SLTU : alu_result = data_a < data_b;
+			`OP_SLT  : alu_result = $signed(data_a) < $signed(data_b);
+			default  : alu_result = 32'bx;
+		endcase
+	end
+	
+endmodule
---
+++ register_file.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//==================================================================================================
+//  Filename      : register_file.v
+//  Created On    : Tue Jan  19 06:40:00 2016
+//  Last Modified : 
+//  Revision      : 0.1
+//  Author        : Emanuel Sánchez & Ninisbeth 
+//  Company       : Universidad Simón Bolívar
+//  Email         : emanuelsab@gmail.com & 
+//
+//  Description   : 32 General Purpose Registers
+//==================================================================================================
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module register_file(
+    input						clk,							// clock
+	 input [4:0] 				reg_addres_a,				//	dirección del registro "a" a leer
+    input [4:0] 				reg_addres_b,				// dirección del registro "b" a leer
+    input [31:0] 				data_d,						// dato a escribir en "d"
+    input [4:0] 				reg_addres_d,				// dirección del registro "d" a escribir
+	 input						ctrl_wb_enable,			// Señal de control para activar la escritura
+    output [31:0] 			data_a,					// dato del registro "a"
+    output [31:0] 			data_b					// dato del registro "a"
+//	   output reg [0:31] 			data_a,					// dato del registro "a"
+//    output reg [0:31] 			data_b					// dato del registro "a"
+    );
+	
+	reg [31:0]					gpregister;
+
+//--------------------------------------------------------------------------
+// Read of Register
+//--------------------------------------------------------------------------
+	//always @(*) begin
+		//if ( reg_addres_a != 0 )
+//			data_a = gpregister[reg_addres_a];
+		//else
+//			data_a = 32'b0;
+		//if ( reg_addres_b != 0 )
+//			data_b = gpregister[reg_addres_b];
+		//else
+//			data_b = 32'b0;
+//	end
+	
+	assign data_a = (reg_addres_a != 0) ? gpregister[reg_addres_a] : 32'b0;
+	assign data_b = (reg_addres_b != 0) ? gpregister[reg_addres_b] : 32'b0;
+
+//--------------------------------------------------------------------------
+// Write in Register: it happens on falling edge
+//--------------------------------------------------------------------------
+	always @( negedge clk ) begin
+		if( reg_addres_d != 5'b0 )
+			if ( ctrl_wb_enable )
+				gpregister[reg_addres_d] <= data_d;
+			else
+				gpregister[reg_addres_d] <= gpregister[reg_addres_d];
+	end
+
+endmodule	
