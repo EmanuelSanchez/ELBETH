40957d16b6f04fe80575fe858bbb9b716e5dafd5
Emanuel <emanuelsab@gmail.com>
2016-03-20 23:33:31 -0430

    updated pc register

--- src/elbeth_pc_register.v
+++ src/elbeth_pc_register.v
@@ -2,7 +2,7 @@
 //==================================================================================================
 //  Filename      : pc_registrer.v
 //  Created On    : Tue Jan  19 06:40:00 2016
-//  Last Modified : 2016-03-07 21:02:21
+//  Last Modified : 2016-03-18 12:25:46
 //  Revision      : 0.1
 //  Author        : Emanuel Sánchez & Ninisbeth 
 //  Company       : Universidad Simón Bolívar
@@ -11,6 +11,7 @@
 //  Description   : Program Counter
 //==================================================================================================
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
 module elbeth_pc_register(
 	input						clk,
 	input						rst,
@@ -20,7 +21,7 @@ module elbeth_pc_register(
     );
 
 	always @(posedge clk) begin
-		pc <= (rst) ? 32'b0 : ((ctrl_stall) ? pc : next_pc);
+		pc <= (rst) ? 32'h200 : ((ctrl_stall) ? pc : next_pc);
 	end
 
 endmodule	//elbeth_pc_register
