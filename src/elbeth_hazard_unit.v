460c1c5a6814bbd3d9bf9a5b681ff348b02c1522
Emanuel <emanuelsab@gmail.com>
2016-03-25 12:14:51 -0430

    fixed problem with hazard unit

--- src/elbeth_hazard_unit.v
+++ src/elbeth_hazard_unit.v
@@ -2,7 +2,7 @@
 //==================================================================================================
 //  Filename      : elbeth_hazard_unit1.v
 //  Created On    : Mon Jan  31 09:46:00 2016
-//  Last Modified : 2016-03-07 21:01:17
+//  Last Modified : 2016-03-25 11:45:38
 //  Revision      : 0.1
 //  Author        : Emanuel Sánchez & Ninisbeth Segovia
 //  Company       : Universidad Simón Bolívar
@@ -22,7 +22,7 @@ module elbeth_hazard_unit (
 	output 			match_forward_rs2
 );
 
-	assign match_forward_rs1 = (exs_rd_addr == exs_rs1) & (exs_rd_addr != `RD_ZERO) & (exs_w_gpr_en);
-	assign match_forward_rs2 = (exs_rd_addr == exs_rs2) & (exs_rd_addr != `RD_ZERO) & (exs_w_gpr_en);
+	assign match_forward_rs1 = (exs_rd_addr == id_rs1) & (exs_rd_addr != `RD_ZERO) & (exs_w_gpr_en);
+	assign match_forward_rs2 = (exs_rd_addr == id_rs2) & (exs_rd_addr != `RD_ZERO) & (exs_w_gpr_en);
 	
 endmodule
